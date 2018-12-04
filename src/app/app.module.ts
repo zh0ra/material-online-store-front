dc74ac4f61ea9b7e879a7e9c54010775b06ff88a
oleksandr.zhorniak <zhora.mail@gmail.com>
2018-12-04 13:54:49 +0100

    add some components in projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import 'hammerjs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+//Services
 
+
+//Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MatToolbarModule
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/constants/app-const.ts
@@ -0,0 +1,7 @@
+const PROTOCOL = 'http';
+const HOSTNAME = '127.0.0.1';
+const PORT = 8181;
+export class AppConst {
+    public static servPath = `${PROTOCOL}://${HOSTNAME}:${PORT}`;
+
+}
