71fe4cf37cd2b189d6e61a3d8bc0f9580222bf57
zhora <zhora.mail@gmail.com>
2019-01-22 19:03:55 +0100

    navbar correction...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,14 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
 import 'hammerjs';
-
 //Material imports
 import { AppMaterialModule } from './app.material.module';
-
 //Components
 import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { UserLoginComponent } from './components/user/user-login/user-login.component';
-import { from } from 'rxjs';
-
 //Services
 import { LoginService } from './services/login.service';
 import { UserService } from './services/user.service';
@@ -24,13 +19,19 @@ import { ProductService } from './services/product.service';
 import { PaymentService } from './services/payment.service';
 import { ShippingService } from './services/shipping.service';
 import { CategoryService } from './services/category.service';
+import { WelcomeComponent } from './components/store/welcome/welcome.component';
+import { ProductListComponent } from './components/store/product-list/product-list.component';
+import { WelcomeCategorySetComponent } from './components/store/welcome-category-set/welcome-category-set.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    UserLoginComponent
+    UserLoginComponent,
+    WelcomeComponent,
+    ProductListComponent,
+    WelcomeCategorySetComponent
   ],
   imports: [
     BrowserModule,
